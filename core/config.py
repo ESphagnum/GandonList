94c5d87d33e9a762f3f46b2b59cfbc6a7f83762d
ESphagnum <sfagnum14@gmail.com>
2025-09-17 17:18:56 +0300

    ADD core/config.py

---
+++ core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    TOKEN : str
+    pg_host : str
+    pg_port : int
+    pg_database : str
+    pg_username : str
+    pg_password : str
+
+    @property
+    def DB_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.pg_username}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_database}"
+    
+    @property
+    def DB_URL_psycopg(self):
+        return f"postgresql+psycopg://{self.pg_username}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_database}"
+
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
